3288896b2751d422fa7746a033c72ef78de76b4b
balajibondar-epam <balaji_bondar@epam.com>
2021-07-28 10:06:05 +0530

    Bugfix-02 changes done

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,5 +2,6 @@ import { LightningElement,api } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     //prcessing logic goes here
+    //Bugfix-02
     @api childAttribute;
 }
